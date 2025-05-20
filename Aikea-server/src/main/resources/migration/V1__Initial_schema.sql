e9054734d5868d43c2de8c9b23890e271ae4ead1
Billie Sautour <billie.sautour@gmail.com>
2025-05-20 15:01:34 +0200

    Refactor application properties and update database schema
    - Change Flyway migration location in application.properties
    - Remove unused Stability AI dependency from pom.xml
    - Add claim_token and token_expiration fields to the user table in the initial schema

--- Aikea-server/src/main/resources/migration/V1__Initial_schema.sql
+++ Aikea-server/src/main/resources/migration/V1__Initial_schema.sql
@@ -8,6 +8,8 @@ create table IF NOT exists users (
     username varchar(100),
     password VARCHAR(100) NOT NULL,
     enabled boolean,
+    claim_token varchar(255),
+    token_expiration datetime,
 primary key (id)) engine=InnoDB;
 
 # On Utilise toujours des liste de roles, pour permettre l'ajout de nouveaux roles a un utilisateur
