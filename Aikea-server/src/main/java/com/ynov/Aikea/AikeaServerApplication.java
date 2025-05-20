e4e2db68abc559b64fb119d1ab1d9fddd7fbf33b
Billie Sautour <billie.sautour@gmail.com>
2025-05-20 10:09:05 +0200

    Init Aikea-server

---
+++ Aikea-server/src/main/java/com/ynov/Aikea/AikeaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.Aikea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikeaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikeaServerApplication.class, args);
+	}
+
+}
---
+++ Aikea-server/src/test/java/com/ynov/Aikea/AikeaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.Aikea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikeaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
